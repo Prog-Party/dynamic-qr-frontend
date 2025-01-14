441f2141a814796148ff6a1dcb7011367065211d
Dennis Rosenbaum <dennis.rosenbaum@outlook.com>
2025-01-14 10:48:01 +0100

    Remove some code for default headers

--- generate-api-client.ts
+++ generate-api-client.ts
@@ -3,42 +3,18 @@ const path = require("path")
 const axios = require("axios")
 
 const generate = async () => {
-  const response = await axios.get("https://dynamic-qrs.azurewebsites.net/api/swagger.json", {
-    headers: {
-      "Authorization": "Bearer YOUR_TOKEN_HERE",
-      "Custom-Header": "CustomValue",
-    },
-  })
-
   try {
     await generateApi({
       name: "api.ts",
       output: path.resolve(process.cwd(), "./src/api/backend"),
       url: "https://dynamic-qrs.azurewebsites.net/api/swagger.json",
-      spec: response.data,
       // 'axios' or or 'fetch'
       httpClientType: "axios",
       generateClient: true,
       generateRouteTypes: true,
       generateResponses: true,
       extraTemplates: [],
-      modular: true,
-      input: {
-        headers: {
-          "Authorization": "Bearer YOUR_TOKEN_HERE",
-          "Custom-Header": "CustomValue",
-        }
-      },
-      hooks: {
-        // You can customize the generation process here
-        onCreateComponent: (component: any) => {
-          return component
-        },
-        onCreateRoute: (routeData: any) => {
-          return routeData
-        },
-
-      },
+      modular: true
     })
 
   } catch (error) {
